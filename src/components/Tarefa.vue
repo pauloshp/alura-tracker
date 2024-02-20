f86d1b687b8364e5102557e266da6892719d554f
pauloshp <henrique.shph@gmail.com>
2024-02-20 14:14:35 -0300

    Adicionando componente Tarefa

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">Descricão da tarefa</div>
+      <div>
+        <Cronometro tempoEmSegundos="15"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Cronometro from './Cronometro.vue'
+
+export default defineComponent({
+  name: 'Tarefa',
+  components: {
+    Cronometro
+  }
+})
+</script>
+
+<style scoped>
+.box {
+  background: #FAF0CA;
+}
+</style>
