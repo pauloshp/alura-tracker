937b2782cd1eb7f8c9a4d036f7112cc592d94089
pauloshp <henrique.shph@gmail.com>
2024-02-22 13:54:39 -0300

    Finalizando projeto

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box">
+  <div class="box formulario" >
     <div class="columns">
       <div class="column" is-8 role="form" aria-label="Formulário para criação de um nova tarefa">
         <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar" v-model="descricao">
@@ -37,3 +37,10 @@ export default defineComponent({
   }
 })
 </script>
+
+<style>
+.formulario {
+  background-color: var(--bg-primario);
+  color: var(--texto-primario);
+}
+</style>
