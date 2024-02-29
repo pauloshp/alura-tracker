b5a9697c822f5add79c238fe264e64aed2d4fffe
pauloshp <henrique.shph@gmail.com>
2024-02-29 20:41:44 -0300

    Implementando filtro de tarefas

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,6 +1,14 @@
 <template>
   <Formulario @aoSalvarTarefa="salvarTarefa"/>
   <div class="lista">
+    <div class="field">
+      <p class="control has-icons-left has-icons-right">
+        <input class="input" type="text" placeholder="Digite para filtrar" v-model="filtro">
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+    </div>
     <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" @aoTarefaClicada="selecionarTarefa"/>
     <Box v-if="listaEstaVazia">
       Você não está muito produtivo hoje :(
@@ -28,7 +36,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, ref, watchEffect } from 'vue';
 import Formulario from '../components/Formulario.vue';
 import Tarefa from '../components/Tarefa.vue';
 import Box from '../components/Box.vue';
@@ -72,9 +80,23 @@ export default defineComponent({
     const store = useStore()
     store.dispatch(OBTER_TAREFAS)
     store.dispatch(OBTER_PROJETOS)
+
+    const filtro = ref("")
+
+    // const tarefas = computed(() => 
+    //   store.state.tarefas.filter(
+    //     (t) => !filtro.value || t.descricao.includes(filtro.value)
+    //   )
+    // )
+
+    watchEffect(() => {
+      store.dispatch(OBTER_TAREFAS, filtro.value)
+    })
+
     return {
       tarefas: computed(() => store.state.tarefas),
-      store
+      store,
+      filtro
     }
   }
 });
