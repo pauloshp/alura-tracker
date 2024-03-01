6a7c1d277537d3837bc30cbf0f3660592878e56d
pauloshp <henrique.shph@gmail.com>
2024-02-29 21:33:04 -0300

    Componentizando a modal

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -13,25 +13,22 @@
     <Box v-if="listaEstaVazia">
       Você não está muito produtivo hoje :(
     </Box>
-    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Editando uma tarefa</p>
-          <button @click="fecharModal" class="delete" aria-label="close"></button>
-        </header>
-        <section class="modal-card-body">
-          <div class="field">
-            <label for="descricaoDaTarefa" class="label"> Descrição </label>
-            <input type="text" class="input" v-model="tarefaSelecionada.descricao" id="descricaoDaTarefa" />
-          </div>
-        </section>
-        <footer class="modal-card-foot">
-          <button @click="alterarTarefa" class="button is-success">Salvar alterações</button>
-          <button @click="fecharModal" class="button">Cancelar</button>
-        </footer>
-      </div>
-    </div>
+    <Modal :mostrar="tarefaSelecionada != null">
+      <template v-slot:cabecalho>
+        <p class="modal-card-title">Editando uma tarefa</p>
+        <button @click="fecharModal" class="delete" aria-label="close"></button>
+      </template>
+      <template v-slot:corpo>
+        <div class="field">
+          <label for="descricaoDaTarefa" class="label"> Descrição </label>
+          <input type="text" class="input" v-model="tarefaSelecionada.descricao" id="descricaoDaTarefa" />
+        </div>
+      </template>
+      <template v-slot:rodape>
+        <button @click="alterarTarefa" class="button is-success">Salvar alterações</button>
+        <button @click="fecharModal" class="button">Cancelar</button>
+      </template>
+    </Modal>
   </div>   
 </template>
 
@@ -40,6 +37,7 @@ import { computed, defineComponent, ref, watchEffect } from 'vue';
 import Formulario from '../components/Formulario.vue';
 import Tarefa from '../components/Tarefa.vue';
 import Box from '../components/Box.vue';
+import Modal from '../components/Modal.vue';
 import { useStore } from '@/store';
 import { ALTERAR_TAREFA, CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS } from '@/store/tipo-acoes';
 import ITarefa from '@/interfaces/ITarefas';
@@ -49,7 +47,8 @@ export default defineComponent({
   components: {
     Formulario,
     Tarefa, 
-    Box
+    Box,
+    Modal
   },
   data () {
     return {
@@ -82,12 +81,6 @@ export default defineComponent({
     store.dispatch(OBTER_PROJETOS)
 
     const filtro = ref("")
-
-    // const tarefas = computed(() => 
-    //   store.state.tarefas.filter(
-    //     (t) => !filtro.value || t.descricao.includes(filtro.value)
-    //   )
-    // )
 
     watchEffect(() => {
       store.dispatch(OBTER_TAREFAS, filtro.value)
